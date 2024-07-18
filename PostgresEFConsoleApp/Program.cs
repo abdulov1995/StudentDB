4c40a0869e5d2565a691e048518fc977053c7506
CodeCraftsman <codecraftsman@example.com>
2024-07-18 18:54:01 +0400

    teacheradding

--- PostgresEFConsoleApp/Program.cs
+++ PostgresEFConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PostgresEFConsoleApp;
+using StudentDB;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +12,21 @@ namespace ConsoleAppWithEF
         static void Main(string[] args)
         {
             StudentService studentService = new StudentService();
+            TeacherService teacherService = new TeacherService();
             //var newStudent = new Student
             //{
             //    Name = "Kamal",
             //    TeacherId = 1
             //};
             //studentService.Create(newStudent);
+
+            //var newTeacher = new Teacher
+            //{
+            //    Id = 2,
+            //    Name = "Irade",
+            //    Subject = "English2"
+            //};
+            //teacherService.Create(newTeacher);
 
 
             //var updatedStudent = new Student
@@ -28,24 +38,56 @@ namespace ConsoleAppWithEF
             //};
             //studentService.Update(2, updatedStudent);
 
+            //var updatedTeacher = new Teacher
+            //{
+            //    Name="Aysel",
+            //    Subject="Cebr"
+            //};
+            //teacherService.Update(2, updatedTeacher);
 
 
             //var students = studentService.GetAll();
             //foreach (var student in students)
             //{
-            //    Console.WriteLine($"Student ID: {student.Id}," +
-            //                         $" Name: {student.Name}, " +
-            //                         $"Age: {student.Age}," +
+            //    Console.WriteLine($"Student ID: {student.Id},"+
+            //                         $" Name: {student.Name},"+
+            //                         $"Age: {student.Age},"+
             //                         $" GPA: {student.GPA}");
             //};
 
+            //var teachers = teacherService.GetAll();
+            //foreach (var teacher in teachers)
+            //{
+            //    Console.WriteLine($"Teacher: {teacher.Name},Subject:{teacher.Subject}");
+            //    foreach (var student in teacher.Students)
+            //    {
+            //        Console.WriteLine($"Student ID: {student.Id}," +
+            //                        $" Name: {student.Name}, " +
+            //                        $"Age: {student.Age}," +
+            //                        $" GPA: {student.GPA}");
+            //    }
+            //};
 
+
+            // teacherService.Delete(2);
 
             ////var student= studentService.GetById(1);
             ////Console.WriteLine(student.Name);
+           
+
+
+
+            //var teacher = teacherService.GetById(1);
+            //Console.WriteLine(teacher.Name);
+            //foreach (var student in teacher.Students)
+            //{
+            //    Console.WriteLine($"Student ID: {student.Id}," +
+            //                       $" Name: {student.Name}, " +
+            //                       $"Age: {student.Age}," +
+            //                       $" GPA: {student.GPA}");
+            //}
 
         }
     }
 
- }
-   
+}
---
+++ PostgresEFConsoleApp/Teacher/TeacherService.cs
@@ -0,0 +1,45 @@
+﻿using PostgresEFConsoleApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentDB
+{
+    public class TeacherService
+    {
+        AppDbContext context = new();
+        public Teacher GetById(int teacherId)
+        {
+            var teacher = context.Teacher.Include(t=>t.Students).FirstOrDefault(s => s.Id == teacherId);
+            return teacher;
+        }
+        public List<Teacher> GetAll()
+        {
+            var teachers = context.Teacher.Include(t => t.Students).ToList();
+            return teachers;
+        }
+        public void Create(Teacher teacher)
+        {
+            context.Teacher.Add(teacher);
+            context.SaveChanges();
+        }
+        public void Update(int id, Teacher updatedTeacher)
+        {
+            var teacher = context.Teacher.FirstOrDefault(t => t.Id == id);
+            teacher.Name = updatedTeacher.Name;
+            teacher.Subject = updatedTeacher.Subject;
+
+            context.Teacher.Update(teacher);
+            context.SaveChanges();
+        }
+        public void Delete(int teacherId)
+        {
+            var teacher = context.Teacher.FirstOrDefault(t => t.Id == teacherId);
+            context.Teacher.Remove(teacher);
+            context.SaveChanges();
+        }
+    }
+}
